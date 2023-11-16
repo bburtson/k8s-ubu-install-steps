62b621bdcfc857a70e28534fff1c68d93af87d4e
Brett A Burtson <brett@burtson.com>
2023-11-15 19:41:21 -0600

    Initial COmmit

---
+++ pre-reqs.sh
@@ -0,0 +1,14 @@
+# Install on all node regardless of control plane or worker node
+
+# containerd
+sudo apt-get install -y containerd
+
+curl -s https://packages.cloud.google.com/apt/
+
+# kubelet 
+
+
+
+# kubeadm
+
+# kubectl
